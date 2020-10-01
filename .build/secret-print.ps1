9b8aa7a98d140bac9ca144f8147711554f46f7e8
Seravy <seravy@gmail.com>
2020-10-01 20:59:28 +0800

    Add a fail to make sure it fails.

--- .build/secret-print.ps1
+++ .build/secret-print.ps1
@@ -1,4 +1,6 @@
 $ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop"
 
 $publishSecret = $Env:PUBLISHSECRET
+Compress-Archive -Path /z/ -DestinationPath .zip -CompressionLevel Optimal
 Write-Host "The secret is '$publishSecret' and arg '$($args[0])'."
