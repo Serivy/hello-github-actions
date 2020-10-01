6279bcde0825dd41ad12e7c7f1c4f9711708c1c3
Seravy <seravy@gmail.com>
2020-10-01 21:05:47 +0800

    Fix error

--- .build/secret-print.ps1
+++ .build/secret-print.ps1
@@ -2,5 +2,5 @@ $ProgressPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
 
 $publishSecret = $Env:PUBLISHSECRET
-Compress-Archive -Path /z/ -DestinationPath .zip -CompressionLevel Optimal
+# Compress-Archive -Path /z/ -DestinationPath .zip -CompressionLevel Optimal
 Write-Host "The secret is '$publishSecret' and arg '$($args[0])'."
